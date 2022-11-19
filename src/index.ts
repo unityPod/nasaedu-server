46acace7c8215109a6735ef8a4cc204e0c9793ec
unityPod <112590118+unityPod@users.noreply.github.com>
2022-11-19 11:05:01 -0800

    Update connection to mongoose and mongo db

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import Express from "express";
+import connectMongoDB from "../connect";
 
 const app = Express();
+connectMongoDB();
+
 
 app.listen(process.env.PORT || 4000, () => {
   console.log("server running")
