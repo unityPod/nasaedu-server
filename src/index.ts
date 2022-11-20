530bb494f033eb9604aa4c360ccc239eaa19fed8
unityPod <112590118+unityPod@users.noreply.github.com>
2022-11-19 20:04:43 -0800

    Update routes

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import connectMongoDB from "./connect";
 import {Seed} from './Seed/seed';
 import {SeedSpirit} from './Seed/seedSpirit';
 import {SeedOpportunity} from './Seed/seedOpportunity';
+import router from "./Routes/routes";
 
 require('dotenv').config();
 
 
 const app = Express();
 connectMongoDB();
+app.use(router)
 // Seed();
 // SeedSpirit();
 // SeedOpportunity();
