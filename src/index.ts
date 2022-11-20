7a0989b8124f0d2c30a248dd9c1b90cc236f4674
unityPod <112590118+unityPod@users.noreply.github.com>
2022-11-19 18:44:58 -0800

    Seed mongo database

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import Express from "express";
 import connectMongoDB from "./connect";
 import {Seed} from './Seed/seed';
+import {SeedSpirit} from './Seed/seedSpirit';
+import {SeedOpportunity} from './Seed/seedOpportunity';
+
 require('dotenv').config();
 
 
 const app = Express();
 connectMongoDB();
-Seed();
+// Seed();
+// SeedSpirit();
+// SeedOpportunity();
 
 
 app.listen(process.env.PORT || 4000, () => {
